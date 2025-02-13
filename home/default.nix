70f666c1d3216d9dd87ff74675edefd12226bf4f
Jean-Pierre Gehrig <jp.gehrig@56k.cloud>
2025-02-13 21:41:52 +0100

    fix homebrew

--- home/default.nix
+++ home/default.nix
@@ -23,9 +23,15 @@
     # You can update Home Manager without changing this value. See
     # the Home Manager release notes for a list of state version
     # changes in each release.
-    stateVersion = "24.05";
+    stateVersion = "24.11";
   };
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
+
+  programs.zsh = {
+    initExtra = ''
+      eval "$(/opt/homebrew/bin/brew shellenv)"
+    '';
+  };
 }
