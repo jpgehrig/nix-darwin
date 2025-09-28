ea2e3c4dbb87604eebbb731e41df88fb1eb5f8d1
Jean-Pierre Gehrig <jp.gehrig@56k.cloud>
2025-09-28 17:33:33 +0200

    quickfix

--- modules/system.nix
+++ modules/system.nix
@@ -174,7 +174,7 @@
       # so it matches common keyboard layout: `ctrl | command | alt`
       #
       # disabled, caused only problems!
-      swap§1LeftCommandAndLeftAlt = false;  
+      swapLeftCommandAndLeftAlt = false;  
     };
   };
 
